63ffb0727080e70595193597aa92e2eab00528b3
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-03-09 19:13:58 +0300

    default interface

--- include/leveldb_queue.h
+++ include/leveldb_queue.h
@@ -1,1 +1,15 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct kvqueue_leveldb_storage_s kvqueue_leveldb_storage_t;
+
+kvqueue_leveldb_storage_t* kvqueue_leveldb_storage_open(const char* path);
+void kvqueue_leveldb_storage_close(kvqueue_leveldb_storage_t* storage);
+
+#ifdef __cplusplus
+}/*extern "C"*/
+#endif
 
--- src/leveldb/queue.c
+++ src/leveldb/queue.c
@@ -1,1 +1,16 @@
+#include <stddef.h>
 
+#include "include/leveldb_queue.h"
+
+struct kvqueue_leveldb_storage_s {
+};
+
+kvqueue_leveldb_storage_t* kvqueue_leveldb_storage_open(const char* path)
+{
+    return NULL;
+}
+
+void kvqueue_leveldb_storage_close(kvqueue_leveldb_storage_t* storage)
+{
+    return;
+}
