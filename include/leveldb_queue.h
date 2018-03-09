45455b26881274cdd55666943309db0bbd8dc222
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-03-09 21:15:26 +0300

    ability to pass custom options

--- include/leveldb_queue.h
+++ include/leveldb_queue.h
@@ -1,13 +1,20 @@
 #pragma once
 
+#include "leveldb/c.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct kvqueue_leveldb_storage_s kvqueue_leveldb_storage_t;
 
-kvqueue_leveldb_storage_t* kvqueue_leveldb_storage_open(const char* path);
-void kvqueue_leveldb_storage_close(kvqueue_leveldb_storage_t* storage);
+kvqueue_leveldb_storage_t* kvqueue_leveldb_storage_create();
+leveldb_options_t* kvqueue_leveldb_default_options();
+const char* kvqueue_leveldb_storage_open( kvqueue_leveldb_storage_t* handler
+                                        , leveldb_options_t*         options
+                                        , const char*                path    );
+void kvqueue_leveldb_storage_close(kvqueue_leveldb_storage_t* handler);
+void kvqueue_leveldb_storage_destroy(kvqueue_leveldb_storage_t* handler);
 
 #ifdef __cplusplus
 }/*extern "C"*/
