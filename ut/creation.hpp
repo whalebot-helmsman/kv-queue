48a3cc2fdf3c10cc4050c6ac3fd9a6ce9624e74a
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-03-09 22:48:02 +0300

    wyou can create storage in normal directory

--- ut/creation.hpp
+++ ut/creation.hpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <gtest/gtest.h>
 
 #include "include/leveldb_queue.h"
@@ -11,4 +12,23 @@ TEST(CreationTest, you_cannot_create_storage_in_root_dir)
                                                                         , path    );
     ASSERT_STRNE(error, NULL);
     kvqueue_leveldb_storage_destroy(storage);
+}
+
+TEST(CreationTest, you_create_storage_in_tmp_dir)
+{
+    const char* path = "/tmp/myqueue";
+    std::string command("rm -rf ");
+    command += path;
+
+    system(command.c_str());
+
+    kvqueue_leveldb_storage_t*  storage =   kvqueue_leveldb_storage_create();
+    const char*                 error   =   kvqueue_leveldb_storage_open( storage
+                                                                        , NULL
+                                                                        , path    );
+    ASSERT_STREQ(error, NULL);
+    kvqueue_leveldb_storage_close(storage);
+    kvqueue_leveldb_storage_destroy(storage);
+
+    system(command.c_str());
 }
